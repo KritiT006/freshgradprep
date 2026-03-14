8986f0bd4916e3bcd707837192e0e70b9d521a77
Shlok <shloksrivastava7@gmail.com>
2026-03-14 20:48:54 +0530

    Soluntion Section boilerplate

---
+++ src/components/SolutionSection.tsx
@@ -0,0 +1,16 @@
+import { Box, Typography } from '@mui/material';
+
+const SolutionSection = () => {
+  return (
+    <Box sx={{ p: 2 }}>
+      <Typography variant="h6" color="text.primary">
+        SolutionSection.tsx
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        Service cards grid
+      </Typography>
+    </Box>
+  );
+};
+
+export default SolutionSection;
